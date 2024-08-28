792ab5d18c7f789b489f8d0f269c8bfd6d6572b2
Dwi Prasetiyo <dwiprasetiyo223@gmail.com>
2024-08-28 16:23:30 +0700

    docs: correct comment

--- src/routers/cart.router.ts
+++ src/routers/cart.router.ts
@@ -5,11 +5,13 @@ import verifyAdminMiddleware from "../middlewares/verify-admin.middleware";
 import authMiddleware from "../middlewares/auth.middleware";
 
 const cartRouter = express.Router();
+// admin
+cartRouter.get("/api/carts", authMiddleware, verifyTokenMiddleware, verifyAdminMiddleware, CartController.get);
+cartRouter.get("/api/carts/products/:productName", authMiddleware, verifyTokenMiddleware, verifyAdminMiddleware, CartController.getByProduct);
+
 // all
 cartRouter.post("/api/carts/items", authMiddleware, verifyTokenMiddleware, CartController.create);
-cartRouter.get("/api/carts", authMiddleware, verifyTokenMiddleware, verifyAdminMiddleware, CartController.get);
 cartRouter.get("/api/carts/users/current", authMiddleware, verifyTokenMiddleware, CartController.getByCurrentUser);
-cartRouter.get("/api/carts/products/:productName", authMiddleware, verifyTokenMiddleware, verifyAdminMiddleware, CartController.getByProduct);
 cartRouter.delete("/api/carts/items/:cartItemId", authMiddleware, verifyTokenMiddleware, CartController.deleteItem);
 
 export default cartRouter;
